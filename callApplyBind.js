b9e58f7adab31e0dc9055a3c9781adffad0a3ccd
Amandeep Singh <amandeep.s@zolostays.com>
2025-03-29 13:55:13 +0530

    apply

--- callApplyBind.js
+++ callApplyBind.js
@@ -6,5 +6,19 @@ const user = {
   name: "Amandeep"
 }
 
+// call - immediately invokes the function with a specified this value and arguments passed one by one.
+
+
+
 // calling greet function by attaching 'user' obj, 'this' inside greet function will point to 'user' object
 greet.call(user, "21", "compABC");
+
+
+// apply - immediately invokes the function, but arguments are passed as an array.
+
+function greetFromCityAndCountry(city, country) {
+  console.log(`Hi I am ${this.name} from ${city}, ${country}`)
+}
+
+greetFromCityAndCountry.apply(user, ["Bangalore", "India"])
+
